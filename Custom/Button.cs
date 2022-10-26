c3d53a780cdb9c1d1afd059c24929bb2178917f5
Alcides <guussaarloos@outlook.com>
2022-10-26 02:30:33 +0200

    Made basic elements for main screen

---
+++ Custom/Button.cs
@@ -0,0 +1,31 @@
+﻿using Checkers.graphics;
+using Raylib_cs;
+using static Raylib_cs.Raylib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkers.Custom
+{
+    public class Button : INode
+    {
+        private Rectangle _rectangle;
+
+        public Button(Vector2 size, Vector2 position)
+        {
+            _rectangle = new Rectangle(position.X, position.Y, position.X + size.X, position.Y + size.Y);
+        }
+        public void Draw()
+        {
+            DrawRectangleRec(_rectangle, Color.BLUE);
+        }
+
+        public void Update()
+        {
+            // TODO: CHECK IF RECTANGLE IS PRESSED
+        }
+    }
+}
---
+++ Screens/MainScreen.cs
@@ -0,0 +1,22 @@
+﻿using Checkers.graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkers.Screens
+{
+    internal class MainScreen : INode
+    {
+        public void Draw()
+        {
+            
+        }
+
+        public void Update()
+        {
+            
+        }
+    }
+}
