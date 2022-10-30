d2e6ec10f76f37beafa1b02c8caf755b4bb20f89
Alcides <guussaarloos@outlook.com>
2022-10-30 20:03:42 +0100

    Revert "Merge branch 'feature/FixWin' into origin/dev"
    This reverts commit 9c5de51713f757c850206dbcd95dce443bbcb2f2.

--- Custom/LoseScreen.cs
+++ Custom/LoseScreen.cs
@@ -30,18 +30,16 @@ namespace Checkers.Custom
         // display of screen.
         private Rectangle _box;
 
-        private byte y = 0;
-
         private Button _exitBtn;
         public LoseScreen()
         {
             _lostSprite = Program.LostSprite;
             _box = new Rectangle((GetScreenWidth() / 2) - 250, (GetScreenHeight() / 2) - 250, 500, 500);
 
-            _position = new Vector2(_box.x + (_box.width / 6), (_box.y + 40));
+            _position = new Vector2(_box.x + (_box.width / 6), (_box.y + 100));
             _frameRec = new Rectangle(0.0f, 0.0f, _width, _height);
 
-            _exitBtn = new Button(new Rectangle(_box.x + (_box.width / 4), _box.y + (_box.height - 60), 250, 50), Color.GREEN, 40, "EXIT GAME");
+            _exitBtn = new Button(new Rectangle(_box.x + (_box.width / 4), _box.y + (_box.height - 50), 250, 50), Color.GREEN, 40, "EXIT GAME");
         }
         public void Draw()
         {
@@ -65,15 +63,12 @@ namespace Checkers.Custom
                 if (_currentFrame > 4)
                 {
                     _currentFrame = 0;
-                    y++;
                     _frameRec.y += (float)(_lostSprite.height / 4);
                 }
 
-                if (_frameRec.y > (_lostSprite.height - (_lostSprite.height / 4)) || y == 3 && _currentFrame == 2)
+                if (_frameRec.y > (_lostSprite.height - (_lostSprite.height / 4)))
                 {
                     _frameRec.y = 0;
-                    _currentFrame = 0;
-                    y = 0;
                 }
 
                 _frameRec.x = (float)_currentFrame * (float)_lostSprite.width / 5;
