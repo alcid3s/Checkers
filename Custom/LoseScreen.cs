4dcd386ead10cfdf21ac42529ee0c83e7735fced
Alcides <guussaarloos@outlook.com>
2022-10-30 18:44:20 +0100

    Minor changes to layout of screens

--- Custom/LoseScreen.cs
+++ Custom/LoseScreen.cs
@@ -30,16 +30,18 @@ namespace Checkers.Custom
         // display of screen.
         private Rectangle _box;
 
+        private byte y = 0;
+
         private Button _exitBtn;
         public LoseScreen()
         {
             _lostSprite = Program.LostSprite;
             _box = new Rectangle((GetScreenWidth() / 2) - 250, (GetScreenHeight() / 2) - 250, 500, 500);
 
-            _position = new Vector2(_box.x + (_box.width / 6), (_box.y + 100));
+            _position = new Vector2(_box.x + (_box.width / 6), (_box.y + 40));
             _frameRec = new Rectangle(0.0f, 0.0f, _width, _height);
 
-            _exitBtn = new Button(new Rectangle(_box.x + (_box.width / 4), _box.y + (_box.height - 50), 250, 50), Color.GREEN, 40, "EXIT GAME");
+            _exitBtn = new Button(new Rectangle(_box.x + (_box.width / 4), _box.y + (_box.height - 60), 250, 50), Color.GREEN, 40, "EXIT GAME");
         }
         public void Draw()
         {
@@ -63,12 +65,15 @@ namespace Checkers.Custom
                 if (_currentFrame > 4)
                 {
                     _currentFrame = 0;
+                    y++;
                     _frameRec.y += (float)(_lostSprite.height / 4);
                 }
 
-                if (_frameRec.y > (_lostSprite.height - (_lostSprite.height / 4)))
+                if (_frameRec.y > (_lostSprite.height - (_lostSprite.height / 4)) || y == 3 && _currentFrame == 2)
                 {
                     _frameRec.y = 0;
+                    _currentFrame = 0;
+                    y = 0;
                 }
 
                 _frameRec.x = (float)_currentFrame * (float)_lostSprite.width / 5;
